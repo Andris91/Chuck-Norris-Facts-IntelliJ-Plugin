4efc7a3a728a07aa4c8e723f49d63a0c6f00688a
Andras Toth <atoth@hpe.com>
2017-02-03 20:41:07 +0200

    Separated code into more classes, plugin will now load a fact at the start.

---
+++ src/main/java/co/atoth/intellij/plugin/chucknorris/FactJTextPane.java
@@ -0,0 +1,56 @@
+package co.atoth.intellij.plugin.chucknorris;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.ui.JBColor;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactJTextPane extends JTextPane {
+
+    private final Logger log = Logger.getInstance(FactService.class);
+
+    private List<Fact> factList = new ArrayList<>();
+    private boolean isLoading;
+
+    public FactJTextPane() {
+        setOpaque(false);
+        setBackground(JBColor.background());
+        setFont(com.intellij.util.ui.UIUtil.getLabelFont().deriveFont(15));
+        setEditable(false);
+        setContentType("text/html");
+    }
+
+    public void setLoading(boolean isLoading) {
+        this.isLoading = isLoading;
+        updateView();
+    }
+
+    private String getHtmlString(List<Fact> facts) {
+        StringBuilder builder = new StringBuilder();
+        if (isLoading) {
+            builder.append("Loading... <hr>");
+        }
+        for (Fact fact : facts) {
+            builder.append(fact.getText() + "<hr>");
+        }
+        return builder.toString();
+    }
+
+    public void addFact(int index, Fact fact) {
+        factList.add(index, fact);
+        updateView();
+    }
+
+    public void addFact(Fact fact) {
+        factList.add(fact);
+        updateView();
+    }
+
+    private void updateView() {
+        setText(getHtmlString(factList));
+    }
+
+
+}
--- src/main/java/co/atoth/intellij/plugin/chucknorris/PluginToolWindowFactory.java
+++ src/main/java/co/atoth/intellij/plugin/chucknorris/PluginToolWindowFactory.java
@@ -1,6 +1,5 @@
 package co.atoth.intellij.plugin.chucknorris;
 
-import com.btr.proxy.search.ProxySearch;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -13,130 +12,84 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Proxy;
-import java.net.ProxySelector;
-import java.net.URI;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PluginToolWindowFactory implements ToolWindowFactory {
 
     private final Logger log = Logger.getInstance(PluginToolWindowFactory.class);
 
     private JTextPane textPane;
-    private List<JSONObject> facts = new ArrayList<>();
+
+    private FactService factService = new FactService();
+    private FactJTextPane factPanel = new FactJTextPane();
+
+    private AnAction getFactAction = new AnAction("Get fact", "Load new Chuck Norris fact", IconLoader.findIcon("/icons/chuck_16x16.png")){
+        @Override
+        public void actionPerformed(AnActionEvent e) {
+            ApplicationManager.getApplication().invokeLater(() -> {
+                factPanel.setLoading(true);
+                e.getPresentation().setEnabled(false);
+
+                Task.Backgroundable backgroundTask = new Task.Backgroundable(null,"Fetching Chuck Norris fact...", false) {
+                    public void run(@NotNull ProgressIndicator indicator) {
+                        factPanel.addFact(0, factService.getRandomFact());
+                    }
+                    @Override
+                    public void onFinished() {
+                        factPanel.setLoading(false);
+                        e.getPresentation().setEnabled(true);
+                    }
+                    @Override
+                    public void onError(@NotNull Exception error) {
+                        factPanel.setLoading(false);
+                        e.getPresentation().setEnabled(true);
+                        super.onError(error);
+                    }
+                };
+                backgroundTask.queue();
+            });
+        }
+    };
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         JPanel rootPanel = new JPanel(new BorderLayout(0, 0));
 
         //Toolbar
-        GetFactAction action = new GetFactAction(project);
         DefaultActionGroup actionGroup = new DefaultActionGroup();
-        actionGroup.add(action);
+        actionGroup.add(getFactAction);
         JComponent component = ActionManager.getInstance().createActionToolbar("chucknorrisplugin", actionGroup, false).getComponent();
-        component.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, JBColor.border()));
         rootPanel.add(component, BorderLayout.WEST);
 
-        //List
-        textPane = new JTextPane();
-        textPane.setOpaque(false);
-        textPane.setBackground(JBColor.background());
-        textPane.setFont(com.intellij.util.ui.UIUtil.getLabelFont().deriveFont(15));
-        textPane.setEditable(false);
-        textPane.setContentType("text/html");
-        JBScrollPane scrollPane = new JBScrollPane(textPane);
+        //Facts
+        JBScrollPane scrollPane = new JBScrollPane(factPanel);
         rootPanel.add(scrollPane, BorderLayout.CENTER);
 
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         toolWindow.getContentManager().addContent(contentFactory.createContent(rootPanel, "", false));
-    }
 
-    private String getHtmlString(List<JSONObject> facts){
-        StringBuilder builder = new StringBuilder();
-
-        for(JSONObject fact : facts){
-            try {
-                if(fact.has("value")) {
-                    JSONObject value = fact.getJSONObject("value");
-                    builder.append(value.get("joke") + "<hr>");
-                }
-            } catch (JSONException ex){
-                log.error(ex);
+        //Load 1 fact at the start
+        factPanel.setLoading(true);
+        Task.Backgroundable backgroundTask = new Task.Backgroundable(null,"Fetching Chuck Norris fact...", false) {
+            public void run(@NotNull ProgressIndicator indicator) {
+                factPanel.addFact(0, factService.getRandomFact());
             }
-        }
-        return builder.toString();
-    }
 
-    public class GetFactAction extends AnAction {
-
-        private static final String API_ADDRESS = "http://api.icndb.com/jokes/random";
-        private Project project;
-
-        public GetFactAction(Project project){
-            super("Get fact", "Load new Chuck Norris fact", IconLoader.findIcon("/icons/chuck_16x16.png"));
-            this.project = project;
-        }
-
-        @Override
-        public void actionPerformed(AnActionEvent e) {
-            ApplicationManager.getApplication().invokeLater(() -> {
-                Task.Backgroundable backgroundTask = new Task.Backgroundable(project, "Fetching Chuck Norris fact...", false) {
-                    public void run(@NotNull ProgressIndicator indicator) {
-                        e.getPresentation().setEnabled(false);
-                        try {
-                            URI uri = URI.create(API_ADDRESS);
-                            URLConnection connection = uri.toURL().openConnection(findProxy());
-                            BufferedReader bR = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                            String line;
-                            StringBuilder responseStrBuilder = new StringBuilder();
-                            while((line =  bR.readLine()) != null){
-                                responseStrBuilder.append(line);
-                            }
-                            JSONObject result = new JSONObject(responseStrBuilder.toString());
-
-                            facts.add(0, result);
-                            textPane.setText(getHtmlString(facts));
-                        } catch (IOException | JSONException ex ) {
-                            log.debug(ex);
-                        } finally {
-                            e.getPresentation().setEnabled(true);
-                        }
-                    }
-                };
-                backgroundTask.queue();
-            });
-        }
-
-        public Proxy findProxy(){
-            ProxySearch proxySearch = new ProxySearch();
-            proxySearch.addStrategy(ProxySearch.Strategy.OS_DEFAULT);
-            proxySearch.addStrategy(ProxySearch.Strategy.JAVA);
-            proxySearch.addStrategy(ProxySearch.Strategy.BROWSER);
-            ProxySelector proxySelector = proxySearch.getProxySelector();
-            ProxySelector.setDefault(proxySelector);
-            URI home = URI.create(API_ADDRESS);
-            log.debug("ProxySelector: " + proxySelector);
-            log.debug("URI: " + home);
-            List<Proxy> proxyList = proxySelector.select(home);
-            if (proxyList != null && !proxyList.isEmpty()) {
-                return proxyList.iterator().next();
+            @Override
+            public void onFinished() {
+                factPanel.setLoading(false);
+                super.onFinished();
             }
-            return Proxy.NO_PROXY;
-        }
+        };
+        backgroundTask.queue();
     }
+
+
+
 
 }
